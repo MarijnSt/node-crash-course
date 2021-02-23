a647d3cf7e37620b641aa3c1ab3e9e41405af979
MarijnSt <marijn.stammeleer@gmail.com>
2021-02-23 22:08:46 +0100

    routing

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const app = express()
 //luister naar reqs
 app.listen(3000)
 
+//res geven
 app.get('/', (req, res) => {
-    res.send('<p>Home</p>')
+    // res.send('<p>Home</p>')
+    res.sendFile('./views/index.html', {
+        root: __dirname
+    })
+})
+app.get('/about', (req, res) => {
+    // res.send('<p>About</p>')
+    res.sendFile('./views/about.html', {
+        root: __dirname
+    })
 })
