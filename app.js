5f409783ad1ee563466ca1ba90a9cfd153576cd8
MarijnSt <marijn.stammeleer@gmail.com>
2021-02-26 21:11:37 +0100

    end lesson

--- app.js
+++ app.js
@@ -9,13 +9,20 @@ app.listen(3000)
 //res geven
 app.get('/', (req, res) => {
     // res.send('<p>Home</p>')
-    res.sendFile('./views/index.html', {
-        root: __dirname
-    })
+    res.sendFile('./views/index.html', { root: __dirname })
 })
+
 app.get('/about', (req, res) => {
     // res.send('<p>About</p>')
-    res.sendFile('./views/about.html', {
-        root: __dirname
-    })
+    res.sendFile('./views/about.html', { root: __dirname })
+})
+
+//redirect
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
+})
+
+//404
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname })
 })
